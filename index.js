8a7e5d00b726d7b39c0feb9dccdb935c6d2c99db
toxin-x <109931081+toxin-x@users.noreply.github.com>
2023-03-28 18:28:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function newtrack() {
             <fieldset class="track">
                 <legend>track ${tracki}:</legend>
                 <label for="track[${tracki}][title]">Title:</label><br>
-                <input type="text" name="track[${tracki}][title]" id="track[${tracki}][title]" class="title" oninput="titledashartist(this)"><br>
+                <input type="text" name="track[${tracki}][title]" id="track[${tracki}][title]" class="title" oninput="titledashartist(this)" required><br>
                 <label for="track[${tracki}][artist]">Artist:</label><br>
-                <input type="text" class="artist" name="track[${tracki}][artist]" id="track[${tracki}][artist]"  oninput="titledashartist(this)"><br>
+                <input type="text" class="artist" name="track[${tracki}][artist]" id="track[${tracki}][artist]"  oninput="titledashartist(this) required"><br>
                 <label for="track[${tracki}][timestamp]">Timestamp (mm:ss):</label><br>
-                <input type="text" name="track[${tracki}][timestamp]" id="track[${tracki}][artist]" pattern="([0-5][0-9]:[0-5][0-9])" placeholder="00:00" title="Must be in mm:ss format"><br>
+                <input type="text" name="track[${tracki}][timestamp]" id="track[${tracki}][artist]" pattern="([0-5][0-9]:[0-5][0-9])" placeholder="00:00" title="Must be in mm:ss format" required><br>
             </fieldset>
 
 
